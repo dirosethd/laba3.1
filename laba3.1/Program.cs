dd84fadd6f9b318627e99708c7efcc64ed3e9fc4
dirose <dirose@DESKTOP-PQA70RD>
2024-10-30 18:57:37 +0200

    Добавьте файлы проекта.

---
+++ laba3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+        for (int i = 1; i <= 1000; i++)
+            if (HasDuplicates(i)) Console.WriteLine(i);
+
+    static bool HasDuplicates(int n)
+    {
+        bool[] seen = new bool[10];
+        foreach (char d in n.ToString())
+            if (seen[d - '0']) return true;
+            else seen[d - '0'] = true;
+        return false;
+    }
+
+}
+catch
+{
+
+}
